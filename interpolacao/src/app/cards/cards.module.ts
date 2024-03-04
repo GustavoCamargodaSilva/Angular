3eb684902776a7071590244f265e6f10d62afeb1
Gustavo Camargo <104240187+GustavoCamargodaSilva@users.noreply.github.com>
2024-03-04 16:10:15 -0300

    criacao do projeto interpolacao

---
+++ interpolacao/src/app/cards/cards.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CardButtonComponent } from '../card-button/card-button.component';
+import { CardRoxoComponent } from '../card-roxo/card-roxo.component';
+import { CardComponent } from '../card/card.component';
+     // Import the missing module here
+
+@NgModule({
+  declarations: [
+    CardComponent,
+    CardButtonComponent,
+    CardRoxoComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    CardComponent,
+    CardRoxoComponent
+    //CardButtonComponent,
+    
+  ]
+})
+export class CardsModule { }
